b323cf8b1d21e9efb8983bfbb0f95ebba7bafcdc
BogdanFront-End <bogdanfreelance26@gmail.com>
2025-10-04 09:20:20 +0300

    Add files via upload

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
-    const headerItems = document.querySelector('.header__items');
-    
-    mobileMenuBtn.addEventListener('click', function() {
-        headerItems.classList.toggle('active');
-        mobileMenuBtn.classList.toggle('active');
-    });
-});
